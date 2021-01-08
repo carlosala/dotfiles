3565b3eed1fbc2302e1186ec1dff08d90977bc11
Carlo Sala <carlosalag@protonmail.com>
2021-01-08 19:17:48 +0100

    Small changes

--- copyconfig.sh
+++ copyconfig.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cp alacritty.yml ~/.config/alacritty/
+cp alacritty/alacritty.yml ~/.config/alacritty/
 
-cp .gitconfig ~/
+cp git/.gitconfig ~/
 
 cp neofetch/config.conf ~/.config/neofetch/
 
 cp nvim/init.vim nvim/maps.vim ~/.config/nvim/
 cp nvim/plugins.vim nvim/plugin-config.vim ~/.config/nvim/
-cp -r nvim/ultisnips ~/.config/nvim/
+cp -r nvim/ultisnips/ ~/.config/nvim/
 
 cp zsh/.zshrc ~/
 cp zsh/nt-modif.zsh-theme zsh/nanotech-24h.zsh-theme ~/.oh-my-zsh/custom/themes/
