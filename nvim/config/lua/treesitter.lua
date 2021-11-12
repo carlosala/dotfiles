4e585e046fd8396864a5867543357da832fea415
Carlo Sala <carlosalag@protonmail.com>
2021-11-12 12:20:43 +0100

    fix(nvim): removing indent option from treesitter

--- nvim/config/lua/treesitter.lua
+++ nvim/config/lua/treesitter.lua
@@ -2,5 +2,4 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained",
   highlight = { enable = true },
   incremental_selection = { enable = true },
-  indent = { enable = true },
 }
