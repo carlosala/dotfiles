d0724044dbd2e33302adbe5cba125a69b56ea372
Carlo Sala <carlosalag@protonmail.com>
2021-12-02 22:53:19 +0100

    feat(git)!: from .gitconfig to .config/git/config

--- copyconfig.sh
+++ copyconfig.sh
@@ -1,12 +1,13 @@
+#!/bin/zsh
 DIR=$(dirname "$0")
 
-cp -r $DIR/alacritty/ ~/.config/
-cp $DIR/git/.gitconfig ~/
-cp -r $DIR/fonts/ ~/.local/share/
-cp -r $DIR/kitty/ ~/.config/
-cp -r $DIR/nvim/ ~/.config/
-cp -r $DIR/zathura/ ~/.config/
-cp $DIR/zsh/.zshrc ~/
-cp $DIR/zsh/nt-modif.zsh-theme ~/.oh-my-zsh/custom/themes/
+cp -r $DIR/alacritty ~/.config
+cp -r $DIR/git ~/.config
+cp -r $DIR/fonts ~/.local/share
+cp -r $DIR/kitty ~/.config
+cp -r $DIR/nvim ~/.config
+cp -r $DIR/zathura ~/.config
+cp $DIR/zsh/.zshrc ~
+cp $DIR/zsh/nt-modif.zsh-theme ~/.oh-my-zsh/custom/themes
 
 echo 'Done!'
