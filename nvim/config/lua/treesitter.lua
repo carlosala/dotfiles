aa65069cbe5d209a2b71c0aea7f6568f25e8f04c
Carlo Sala <carlosalag@protonmail.com>
2021-12-05 16:46:05 +0100

    feat(nvim): adding treesitter indent

--- nvim/config/lua/treesitter.lua
+++ nvim/config/lua/treesitter.lua
@@ -2,5 +2,6 @@ require("nvim-treesitter.configs").setup({
   ensure_installed = "maintained",
   highlight = { enable = true },
   incremental_selection = { enable = true },
+  indent = { enable = true },
   autotag = { enable = true },
 })
