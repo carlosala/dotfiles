965457d520c32d74f5c3075e32cd713a55824082
Carlo Sala <carlosalag@protonmail.com>
2021-12-07 12:51:24 +0100

    feat(fonts): adding patcher and fontconfig folders
    updating JetBrainsMono to 2.251

--- copyconfig.sh
+++ copyconfig.sh
@@ -3,7 +3,8 @@ DIR=$(dirname "$0")
 
 cp -r $DIR/alacritty ~/.config
 cp -r $DIR/git ~/.config
-cp -r $DIR/fonts ~/.local/share
+cp -r $DIR/fonts/fontconfig ~/.config
+cp -r $DIR/fonts/fonts ~/.local/share
 cp -r $DIR/kitty ~/.config
 cp -r $DIR/nvim ~/.config
 cp -r $DIR/zathura ~/.config
