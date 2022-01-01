183155c2e88112f9b6d4cff910a0dff55b481118
Carlo Sala <carlosalag@protonmail.com>
2022-01-01 20:19:29 +0100

    feat(installer): improvements

--- installer.py
+++ installer.py
@@ -3,51 +3,30 @@ import os
 import subprocess
 
 
-def checkingPackage(package: str):
-    return f"Checking if {package} is installed..."
-
-
-def notInstalled(package: str):
-    return f"{package} is not installed!"
-
-
 def neededPackage(app: str, package: str):
-    return f"{package} is needed for {app}. Do you want to install it now? [Y/n] "
-
-
-def notRequiredNotInstalled(package: str):
-    return f"{package} installation skipped."
-
-
-def notRequiredFailed(package: str):
-    return f"{package} installation failed. Skipping..."
-
-
-welcome = "Hi! You are in Carlo's dotfiles installer :)\nYou are going to be asked for some things so, let's do it!\n"
-
-zshNoDefault = "Zsh is not your default shell. This dotfiles are intended to be used with zsh. If you want to use it, please execute $chsh -s /bin/zsh."
-
-rustNvimUtilities = "You will need some rust utilities necessary for nvim usage. Them are bat, fd-find and ripgrep. You will be asked for each one."
-
-theEnd = (
-    "\nYou are ready to go!\nHave a nice day, and thank you for using my dotfiles ;)"
-)
+    if app != "":
+        return f"{package} is needed for {app}. Do you want to install it now? [Y/n] "
+    return (
+        f"{package} is not needed, but I use it. Do you want to install it now? [Y/n] "
+    )
 
 
 def checkPackage(package: str):
-    print(checkingPackage(package))
+    print(f"Checking if {package} is installed...")
     try:
         subprocess.run([package, "--version"], capture_output=True)
         print("Done!\n")
         return 0
     except:
-        print(f"{notInstalled(package)}")
+        print(f"{package} is not installed!")
         return 1
 
 
 def checkZshDefault():
     if os.environ["SHELL"].endswith("zsh") == False:
-        print(zshNoDefault)
+        print(
+            "Zsh is not your default shell. This dotfiles are intended to be used with zsh, if you want to use it, please execute $chsh -s /bin/zsh."
+        )
     return 0
 
 
@@ -57,13 +36,13 @@ def installPackage(app: str, package: str, command: list[str]):
         print("\n")
         try:
             subprocess.run(command)
-            return 1
+            return True
         except:
-            print(notRequiredFailed(i))
-            return 0
+            print(f"{package} installation failed. Skipping...")
+            return False
     else:
-        print(notRequiredNotInstalled(package))
-        return 0
+        print(f"{package} installation skipped.")
+        return False
 
 
 def checkingBasicRequirements():
@@ -86,7 +65,7 @@ def installingPackages():
         "nvim", "neovim-remote (pip)", ["pip3", "install", "--user", "neovim-remote"]
     )
     yarnResult = installPackage("nvim", "yarn", ["npm", "install", "-g", "yarn"])
-    if yarnResult == 1:
+    if yarnResult:
         installPackage(
             "nvim", "neovim (node integration)", ["yarn", "global", "add", "neovim"]
         )
@@ -95,18 +74,23 @@ def installingPackages():
             "nvim", "neovim (node integration)", ["npm", "install", "-g", "neovim"]
         )
 
-    print(rustNvimUtilities)
     installPackage("nvim", "bat", ["cargo", "install", "bat"])
-    installPackage("nvim", "fd-find", ["cargo", "install", "fd-find"])
+    installPackage("nvim", "fd", ["cargo", "install", "fd-find"])
     installPackage("nvim", "ripgrep", ["cargo", "install", "ripgrep"])
     installPackage("zsh", "exa", ["cargo", "install", "exa"])
+    installPackage("", "dust", ["cargo", "install", "du-dust"])
+    installPackage("", "hyperfine", ["cargo", "install", "hyperfine"])
 
 
 def main():
-    print(welcome)
+    print(
+        "Hi! You are in Carlo's dotfiles installer :)\nYou are going to be asked for some things so, let's do it!\n"
+    )
     checkingBasicRequirements()
     installingPackages()
-    print(theEnd)
+    print(
+        "\nYou are ready to go!\nHave a nice day, and thank you for using my dotfiles ;)"
+    )
 
 
 if __name__ == "__main__":
