7d75c1e154547404660e4fa8dab3a7564f19e9c6
Carlo Sala <carlosalag@protonmail.com>
2022-03-09 13:12:23 +0100

    feat(fonts): switch to non-patched fonts
    kitty released v0.24.3 that solves the problem of huge symbols when
    using the only-symbols nerd font. Now it's working🎉

--- copyconfig.sh
+++ copyconfig.sh
@@ -4,8 +4,7 @@ DIR=${0:h}
 cp -r $DIR/alacritty ~/.config
 cp -r $DIR/bat ~/.config
 cp -r $DIR/git ~/.config
-cp -r $DIR/fonts/fonts ~/.local/share
-cp -r $DIR/fonts/fontconfig ~/.config
+cp -r $DIR/fonts ~/.local/share
 cp -r $DIR/kitty ~/.config
 cp -r $DIR/latexmk ~/.config
 cp -r $DIR/nvim ~/.config
