67c9fdda956ba1f597dd0c311be05895a4d29258
Carlo Sala <carlosalag@protonmail.com>
2022-08-12 17:00:57 +0100

    feat(nvim): use native lua to create custom commands

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -1,12 +1,13 @@
-vim.cmd([[
-command! -bang -nargs=? -complete=dir FilesCustom
-  \ call fzf#vim#files(<q-args>, fzf#vim#with_preview({
-  \ "source": "fd -t f -i -H -E .git",
-  \ "options": "--info=inline"}), <bang>0)
+local addCmd = vim.api.nvim_create_user_command
 
-command! -bang -nargs=* RgCustom
-  \ call fzf#vim#grep(
-  \ 'rg -n --color=always -i --hidden -g "!*.git/*"
-  \ -g "!yarn.lock" -g "!package-lock.json" -- '.shellescape(<q-args>),
-  \ 1, fzf#vim#with_preview({"options": "--info=inline"}), <bang>0)
-]])
+addCmd(
+  "FilesCustom",
+  [[call fzf#vim#files(<q-args>, fzf#vim#with_preview({ "source": "fd -t f -i -H -E .git", "options": "--info=inline" }), <bang>0)]],
+  { bang = true, complete = "dir", nargs = "?" }
+)
+
+addCmd(
+  "RgCustom",
+  [[call fzf#vim#grep('rg -n --color=always -i --hidden -g "!*.git/*" -g "!yarn.lock" -g "!package-lock.json" -- '.shellescape(<q-args>), 1, fzf#vim#with_preview({"options": "--info=inline"}), <bang>0)]],
+  { bang = true, nargs = "?" }
+)
