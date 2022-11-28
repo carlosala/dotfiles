8ac244d215328dad95cafbba02231424d08dc1c4
Carlo Sala <carlosalag@protonmail.com>
2022-11-28 19:37:38 +0100

    feat(nvim): add mapping to abort completion

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -30,7 +30,13 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-Space>"] = cmp.mapping(function()
+      if cmp.visible() then
+        return cmp.abort()
+      else
+        cmp.complete()
+      end
+    end),
     ["<CR>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         return cmp.confirm({ select = true })
