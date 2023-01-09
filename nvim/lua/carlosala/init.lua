25ac6025bc060f489b661f974a7ac64f16214910
Carlo Sala <carlosalag@protonmail.com>
2023-01-09 14:32:06 +0100

    fix(nvim): require `impatient` in protected mode

--- nvim/lua/carlosala/init.lua
+++ nvim/lua/carlosala/init.lua
@@ -1,4 +1,4 @@
-require("impatient") -- speed up startuptime
+pcall(require, "impatient") -- speed up startuptime
 require("carlosala.options")
 require("carlosala.viml_config")
 require("carlosala.packer")
