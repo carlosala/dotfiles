126f68bdbd30ba9a472a13d08afdbf32e85d84a5
Carlo Sala <carlosalag@protonmail.com>
2023-01-30 09:55:07 +0100

    feat(nvim): switch to new nvim-tree startup system

--- nvim/after/plugin/navigation.lua
+++ nvim/after/plugin/navigation.lua
@@ -18,3 +18,24 @@ require("nvim-tree").setup({
   actions = { open_file = { quit_on_open = true } },
   git = { ignore = false },
 })
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  callback = function(data)
+    -- check if it's a directory
+    if vim.fn.isdirectory(data.file) ~= 1 then
+      return
+    end
+
+    -- create a new, empty buffer
+    vim.cmd.enew()
+
+    -- wipe the directory buffer
+    vim.cmd.bw(data.buf)
+
+    -- cd to the directory
+    vim.cmd.cd(data.file)
+
+    -- open the tree in the current buffer
+    require("nvim-tree.api").tree.open({ current_window = true })
+  end,
+})
