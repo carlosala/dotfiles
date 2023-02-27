4ad83c3fbce009d2bc243542c36528b179094758
Carlo Sala <carlosalag@protonmail.com>
2023-02-27 13:28:58 +0100

    feat(nvim): add latexindent `-m` flag

--- nvim/lua/carlosala/viml_config.lua
+++ nvim/lua/carlosala/viml_config.lua
@@ -1,6 +1,9 @@
 -- editorconfig-vim
 vim.g.EditorConfig_exclude_patterns = { "fugitive://.*" }
 
+-- neoformat
+vim.g.latexindent_opt = "-m"
+
 -- nvim-r
 vim.g.R_auto_start = 2
 vim.g.R_auto_omni = {}
