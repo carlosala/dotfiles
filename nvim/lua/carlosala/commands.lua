233c5afd33ea76a5f34ee7c58f0a88bbbe041669
Carlo Sala <carlosalag@protonmail.com>
2023-04-24 09:04:26 +0200

    fix(nvim): avoid hit-enter message in :TexWordCount

--- nvim/lua/carlosala/commands.lua
+++ nvim/lua/carlosala/commands.lua
@@ -1,10 +1,11 @@
 local function _word_count()
   if vim.bo.filetype ~= "tex" then
+    vim.notify("This command is only available for Tex files")
     return
   end
   local _fn = vim.fn.expand("%")
-  local _count = vim.fn.system("texcount " .. _fn .. " -inc -incbib -sum -1")
-  vim.cmd.echo(_count)
+  local _count = tonumber(vim.fn.system("texcount " .. _fn .. " -inc -incbib -sum -1"))
+  vim.notify("Word count: " .. tostring(_count))
 end
 
 vim.api.nvim_create_user_command("TexWordCount", _word_count, {})
