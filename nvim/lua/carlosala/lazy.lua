e3491493cb143069dd0991ec2ce268e5a7001c50
Carlo Sala <carlosalag@protonmail.com>
2023-05-02 11:41:03 +0200

    fix(nvim): disable change detection notify in lazy.nvim

--- nvim/lua/carlosala/lazy.lua
+++ nvim/lua/carlosala/lazy.lua
@@ -11,4 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("carlosala.plugins")
+require("lazy").setup("carlosala.plugins", {
+  change_detection = { notify = false },
+  install = { colorscheme = { "gruvbox-material" } },
+})
