d5b0f3d9ecf31668b2c45c510e9989839cf26bfb
Carlo Sala <carlosalag@protonmail.com>
2023-05-21 17:44:13 +0200

    fix(nvim): latex inverse search

--- nvim/lua/carlosala/plugins/lsp.lua
+++ nvim/lua/carlosala/plugins/lsp.lua
@@ -82,7 +82,7 @@ return {
             forwardSearch = {
               args = {
                 "--synctex-editor-command",
-                "nvim-texlabconfig -file '%{input}' -line %{line}",
+                "nvim-texlabconfig -file '%%{input}' -line %%{line} -server " .. vim.v.servername,
                 "--synctex-forward",
                 "%l:1:%f",
                 "%p",
