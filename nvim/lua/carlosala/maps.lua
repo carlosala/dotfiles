6812df218406c69aeebee5cbb1567517ac8fffe4
Carlo Sala <carlosalag@protonmail.com>
2023-08-09 19:23:38 +0200

    feat(nvim): replace `neoformat` by `formatter.nvim`

--- nvim/lua/carlosala/maps.lua
+++ nvim/lua/carlosala/maps.lua
@@ -37,7 +37,7 @@ map("n", "]d", vim.diagnostic.goto_next, silent)
 -- plugins
 map("n", "<Leader>n", vim.cmd.NvimTreeFindFile, silent)
 map("n", "<Leader>u", vim.cmd.UndotreeToggle, silent)
-map({ "n", "v" }, "<Leader>b", ":Neoformat<CR>", silent)
+map({ "n", "v" }, "<Leader>b", ":Format<CR>", silent)
 map("n", "<Leader>vd", ":Gvdiffsplit!<CR>", silent)
 
 -- telescope
