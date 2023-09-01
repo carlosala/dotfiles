1405c139e6214eff902a8749d9e20f31749ca916
Carlo Sala <carlosalag@protonmail.com>
2023-09-01 12:12:57 +0200

    feat(nvim): add `nvim-lint` and reorganize code

---
+++ nvim/lua/carlosala/plugins/lint-format.lua
@@ -0,0 +1,58 @@
+return {
+  {
+    "mhartington/formatter.nvim",
+    cmd = { "Format", "FormatWrite", "FormatLock", "FormatWriteLock" },
+    config = function()
+      require("formatter").setup({
+        log_level = vim.log.levels.INFO,
+        filetype = {
+          c = { require("formatter.filetypes.c").clangformat },
+          cpp = { require("formatter.filetypes.cpp").clangformat },
+          javascript = { require("formatter.filetypes.javascript").prettier },
+          json = { require("formatter.filetypes.json").prettier },
+          jsonc = { require("formatter.filetypes.json").prettier },
+          lua = { require("formatter.filetypes.lua").stylua },
+          markdown = { require("formatter.filetypes.markdown").prettier },
+          python = { require("formatter.filetypes.python").black },
+          rust = { require("formatter.filetypes.rust").rustfmt },
+          typescript = { require("formatter.filetypes.typescript").prettier },
+          vue = { require("formatter.filetypes.vue").prettier },
+          yaml = { require("formatter.filetypes.yaml").prettier },
+          bib = {
+            function()
+              return {
+                exe = "bibclean",
+                stdin = true,
+              }
+            end,
+          },
+          tex = {
+            function()
+              return {
+                exe = "latexindent",
+                args = { "-g", "/dev/null", "-m", "-rv" },
+                stdin = true,
+              }
+            end,
+          },
+        },
+      })
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    config = function()
+      require("lint").linters_by_ft = {
+        c = { "clangtidy" },
+        cpp = { "clangtidy" },
+        lua = { "luacheck" },
+        python = { "pylint" },
+      }
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+    end,
+  },
+}
--- nvim/lua/carlosala/plugins/misc.lua
+++ nvim/lua/carlosala/plugins/misc.lua
@@ -1,44 +1,5 @@
 return {
   {
-    "mhartington/formatter.nvim",
-    cmd = { "Format", "FormatWrite", "FormatLock", "FormatWriteLock" },
-    config = function()
-      require("formatter").setup({
-        log_level = vim.log.levels.INFO,
-        filetype = {
-          c = { require("formatter.filetypes.c").clangformat },
-          cpp = { require("formatter.filetypes.cpp").clangformat },
-          javascript = { require("formatter.filetypes.javascript").prettier },
-          json = { require("formatter.filetypes.json").prettier },
-          jsonc = { require("formatter.filetypes.json").prettier },
-          lua = { require("formatter.filetypes.lua").stylua },
-          markdown = { require("formatter.filetypes.markdown").prettier },
-          python = { require("formatter.filetypes.python").black },
-          rust = { require("formatter.filetypes.rust").rustfmt },
-          typescript = { require("formatter.filetypes.typescript").prettier },
-          yaml = { require("formatter.filetypes.yaml").prettier },
-          bib = {
-            function()
-              return {
-                exe = "bibclean",
-                stdin = true,
-              }
-            end,
-          },
-          tex = {
-            function()
-              return {
-                exe = "latexindent",
-                args = { "-g", "/dev/null", "-m", "-rv" },
-                stdin = true,
-              }
-            end,
-          },
-        },
-      })
-    end,
-  },
-  {
     "jalvesaq/nvim-r",
     ft = "r",
     init = function()
