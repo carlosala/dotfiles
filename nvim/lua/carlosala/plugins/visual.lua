5fdafc78339f2d33840705a43a589d4784acebe1
Carlo Sala <carlosalag@protonmail.com>
2023-10-01 11:31:42 +0200

    fix(nvim): update to `indent-blankline` v3

--- nvim/lua/carlosala/plugins/visual.lua
+++ nvim/lua/carlosala/plugins/visual.lua
@@ -19,10 +19,8 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     event = "VeryLazy",
-    opts = {
-      show_current_context = true,
-      use_treesitter = true,
-    },
+    main = "ibl",
+    config = true,
   },
   {
     "nvim-lualine/lualine.nvim",
