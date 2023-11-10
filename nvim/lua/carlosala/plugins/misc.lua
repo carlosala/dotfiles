69aad2facf138db84e94a81da3637cf59b1267a4
Carlo Sala <carlosalag@protonmail.com>
2023-11-10 12:02:56 +0100

    feat(nvim): add `markdown-preview.nvim` plugin

--- nvim/lua/carlosala/plugins/misc.lua
+++ nvim/lua/carlosala/plugins/misc.lua
@@ -1,5 +1,10 @@
 return {
   {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && yarn install",
+    ft = "markdown",
+  },
+  {
     "jalvesaq/nvim-r",
     ft = "r",
     init = function()
