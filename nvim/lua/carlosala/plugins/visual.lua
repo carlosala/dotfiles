8456e36718cc55e72a47fc6ce8f87d2505ccab9f
Carlo Sala <carlosalag@protonmail.com>
2023-11-15 11:21:53 +0100

    fix(nvim): highlight properly `DiagnosticDeprecated`

--- nvim/lua/carlosala/plugins/visual.lua
+++ nvim/lua/carlosala/plugins/visual.lua
@@ -11,9 +11,11 @@ return {
       vim.g.gruvbox_material_background = "soft"
       vim.g.gruvbox_material_better_performance = 1
       vim.g.gruvbox_material_diagnostic_text_highlight = 1
+      vim.g.gruvbox_material_diagnostic_virtual_text = "highlighted"
       vim.g.gruvbox_material_enable_italic = 1
       vim.g.gruvbox_material_transparent_background = 1
       vim.cmd.colorscheme("gruvbox-material")
+      vim.cmd("highlight! link DiagnosticDeprecated HintText")
     end,
   },
   {
