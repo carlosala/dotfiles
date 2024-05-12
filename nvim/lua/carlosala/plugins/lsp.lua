39454648cb9155a5a27aeb0307232b42ace57d33
Carlo Sala <carlosalag@protonmail.com>
2024-05-12 16:13:51 +0200

    Revert "fix(nvim): `tsserver` binary is not required"
    This reverts commit bd872873af8be6df873da256685cd9ab28b806bd.

--- nvim/lua/carlosala/plugins/lsp.lua
+++ nvim/lua/carlosala/plugins/lsp.lua
@@ -17,6 +17,7 @@ return {
       require("mason").setup()
       require("mason-lspconfig").setup({
         automatic_installation = { exclude = { "clangd", "r_language_server" } },
+        ensure_installed = { "tsserver" },
       })
       require("mason-tool-installer").setup({
         ensure_installed = { "luacheck", "prettier", "stylua", "taplo" },
