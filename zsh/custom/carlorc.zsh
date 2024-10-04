2ebd1946c969a4b163b9f749b363926a47abb1e3
Carlo Sala <carlosalag@protonmail.com>
2024-10-04 14:41:07 +0200

    feat(zsh): source `carlorc` from a subdirectory

--- zsh/custom/carlorc.zsh
+++ zsh/custom/carlorc.zsh
@@ -1,6 +1,14 @@
 function _carlorc_hook {
-  [[ -f .carlorc ]] || builtin return 0
-  local _script=".carlorc"
+  local _pwd=$PWD
+  typeset -ga _carlorc_visited
+  while [[ $_pwd != "/" ]]; do
+    (( ${_carlorc_visited[(Ie)$_pwd]} )) && return 0
+    _carlorc_visited+=($_pwd)
+    [[ -f "$_pwd/.carlorc" ]] && break
+    _pwd=${_pwd:h}
+  done
+  local _script="$_pwd/.carlorc"
+  [[ -f $_script ]] || builtin return 0
 
   # ensure config file exists and is valid
   local _dir="${XDG_CONFIG_HOME:-${HOME}/.config}/carlorc"
@@ -10,25 +18,19 @@ function _carlorc_hook {
     builtin echo "{}" > "$_file"
   fi
 
-  local _entry=$(command jq -r ".\"$PWD\"" "$_file")
+  local _entry=$(command jq -r ".\"$_pwd\"" "$_file")
   local _sum=$(command sha1sum "$_script" | awk '{ print $1 }')
-  local _exec=0
-  typeset -ga _carlorc_sourced
 
   if [[ $_sum != $_entry ]]; then
     if builtin read -q "?Do you want to execute .carlorc and whitelist its content? [y/N] "; then
-      _exec=1
+      builtin echo
+      return 1
     fi
     builtin echo
-  else
-    (( ${_carlorc_sourced[(Ie)$PWD]} )) || _exec=1
   fi
 
-  if (( ${_exec} )); then
-    (( ${_carlorc_sourced[(Ie)$PWD]} )) || _carlorc_sourced+=($PWD)
-    builtin echo $(command jq ". += { \"$PWD\": \"$_sum\" }" "$_file") > "$_file"
-    builtin source $_script
-  fi
+  builtin echo $(command jq ". += { \"$_pwd\": \"$_sum\" }" "$_file") > "$_file"
+  builtin source $_script
 }
 
 autoload -U add-zsh-hook
