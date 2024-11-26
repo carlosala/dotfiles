7fd527f57cd510cdec078f1219912e4bc19b01e4
Carlo Sala <carlosalag@protonmail.com>
2024-11-26 11:42:49 +0100

    fix(nvim): highlight TODOS without colon

--- nvim/lua/carlosala/plugins/navigation.lua
+++ nvim/lua/carlosala/plugins/navigation.lua
@@ -55,7 +55,10 @@ return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     event = "VeryLazy",
-    config = true,
+    opts = {
+      highlight = { pattern = [[.*<(KEYWORDS)\s*]] },
+      search = { pattern = [[\b(KEYWORDS)\b]] },
+    },
   },
   {
     "knubie/vim-kitty-navigator",
