0ab7962e9310e46f61eac6a73c41aab4c6ef2671
Carlo Sala <carlosalag@protonmail.com>
2025-12-09 13:04:40 +0100

    fix(nvim): ensure global `on_attach` function is respected

--- nvim/lua/carlosala/plugins/lsp.lua
+++ nvim/lua/carlosala/plugins/lsp.lua
@@ -51,33 +51,51 @@ return {
         require("cmp_nvim_lsp").default_capabilities()
       )
 
+      --- @type fun(client: vim.lsp.Client, bufnr: integer)
+      local function common_on_attach(_, bufnr)
+        local bufopts = { noremap = true, silent = true, buffer = bufnr }
+        local map = vim.keymap.set
+        map("n", "gd", "<Cmd>Telescope lsp_definitions<CR>", bufopts)
+        map("n", "gi", "<Cmd>Telescope lsp_implementations<CR>", bufopts)
+        map("n", "gy", "<Cmd>Telescope lsp_type_definitions<CR>", bufopts)
+        map("n", "gr", "<Cmd>Telescope lsp_references<CR>", bufopts)
+        map("n", "<Leader>rn", vim.lsp.buf.rename, bufopts)
+        map("n", "<Leader>ca", function()
+          vim.lsp.buf.code_action({ apply = true })
+        end, bufopts)
+        map("n", "<Leader>sd", "<Cmd>Telescope lsp_document_symbols<CR>", bufopts)
+        map("n", "<Leader>sw", "<Cmd>Telescope lsp_dynamic_workspace_symbols<CR>", bufopts)
+      end
+
       --- @class (partial) Config.P: vim.lsp.Config
-      --- @param  custom_config? Config.P
-      --- @param custom_on_attach? vim.lsp.client.on_attach_cb
-      --- @return vim.lsp.Config
-      local function config(custom_config, custom_on_attach)
-        return vim.tbl_deep_extend("force", {
+      --- @param server string
+      --- @param custom_config? Config.P
+      --- @param custom_on_attach? fun(client: vim.lsp.Client, bufnr: integer)
+      local function lsp_enable(server, custom_config, custom_on_attach)
+        local default_config = vim.lsp.config[server] or {}
+        local config = vim.tbl_deep_extend("force", default_config, {
           capabilities = vim.deepcopy(default_capabilities),
-          --- @type vim.lsp.client.on_attach_cb
           on_attach = function(client, bufnr)
-            local bufopts = { noremap = true, silent = true, buffer = bufnr }
-            local map = vim.keymap.set
-            map("n", "gd", "<Cmd>Telescope lsp_definitions<CR>", bufopts)
-            map("n", "gi", "<Cmd>Telescope lsp_implementations<CR>", bufopts)
-            map("n", "gy", "<Cmd>Telescope lsp_type_definitions<CR>", bufopts)
-            map("n", "gr", "<Cmd>Telescope lsp_references<CR>", bufopts)
-            map("n", "<Leader>rn", vim.lsp.buf.rename, bufopts)
-            map("n", "<Leader>ca", function()
-              vim.lsp.buf.code_action({ apply = true })
-            end, bufopts)
-            map("n", "<Leader>sd", "<Cmd>Telescope lsp_document_symbols<CR>", bufopts)
-            map("n", "<Leader>sw", "<Cmd>Telescope lsp_dynamic_workspace_symbols<CR>", bufopts)
+            local def_on_attach = default_config.on_attach
+            if def_on_attach then
+              if type(def_on_attach) == "function" then
+                def_on_attach = { def_on_attach }
+              end
+              for _, v in ipairs(def_on_attach) do
+                v(client, bufnr)
+              end
+            end
+
+            common_on_attach(client, bufnr)
 
             if custom_on_attach then
               custom_on_attach(client, bufnr)
             end
           end,
         }, custom_config or {})
+
+        vim.lsp.config(server, config)
+        vim.lsp.enable(server)
       end
 
       local function get_python_path(root_dir)
@@ -102,7 +120,7 @@ return {
       ---@type rustaceanvim.Opts
       vim.g.rustaceanvim = {
         server = {
-          on_attach = config().on_attach,
+          on_attach = common_on_attach,
           default_settings = {
             ["rust-analyzer"] = {
               cachePriming = { numThreads = tonumber(vim.env.CARGO_BUILD_JOBS) },
@@ -134,75 +152,55 @@ return {
         },
       }
 
-      local function lsp_enable(server, conf)
-        vim.lsp.config(server, conf)
-        vim.lsp.enable(server)
-      end
+      lsp_enable("eslint")
+      lsp_enable("lua_ls")
+      lsp_enable("r_language_server")
+      lsp_enable("ruff")
+      lsp_enable("svelte")
+      lsp_enable("tailwindcss")
+      lsp_enable("ts_ls")
 
-      lsp_enable("eslint", config())
-      lsp_enable("lua_ls", config())
-      lsp_enable("r_language_server", config())
-      lsp_enable("ruff", config())
-      lsp_enable("svelte", config())
-      lsp_enable("tailwindcss", config())
-      lsp_enable("ts_ls", config())
-
-      lsp_enable(
-        "clangd",
-        config(nil, function(_, bufnr)
-          vim.keymap.set(
-            "n",
-            "<LocalLeader>ls",
-            "<Cmd>ClangdSwitchSourceHeader<CR>",
-            { noremap = true, silent = true, buffer = bufnr }
-          )
-        end)
-      )
-      lsp_enable(
-        "jsonls",
-        config({
-          settings = {
-            json = {
-              schemas = require("schemastore").json.schemas(),
-              validate = { enable = true },
-            },
+      lsp_enable("clangd", nil, function(_, bufnr)
+        vim.keymap.set(
+          "n",
+          "<LocalLeader>ls",
+          "<Cmd>ClangdSwitchSourceHeader<CR>",
+          { noremap = true, silent = true, buffer = bufnr }
+        )
+      end)
+      lsp_enable("jsonls", {
+        settings = {
+          json = {
+            schemas = require("schemastore").json.schemas(),
+            validate = { enable = true },
           },
-        })
-      )
-      lsp_enable(
-        "pyright",
-        config({
-          before_init = function(_, conf)
-            conf.settings.python.pythonPath = get_python_path(conf.root_dir)
-          end,
-        })
-      )
-      lsp_enable(
-        "texlab",
-        config({
-          settings = { texlab = { chktex = { onOpenAndSave = true } } },
-        }, function(client, bufnr)
-          ---@diagnostic disable-next-line: assign-type-mismatch
-          client.server_capabilities.completionProvider = false -- we use `vimtex` completion!
-          vim.keymap.set(
-            "n",
-            "<LocalLeader>lw",
-            "<Cmd>w<CR><Cmd>TexWordCount<CR>",
-            { noremap = true, silent = true, buffer = bufnr }
-          )
-        end)
-      )
-      lsp_enable(
-        "yamlls",
-        config({
-          settings = {
-            yaml = {
-              schemaStore = { enable = false, url = "" },
-              schemas = require("schemastore").yaml.schemas(),
-            },
+        },
+      })
+      lsp_enable("pyright", {
+        before_init = function(_, conf)
+          conf.settings.python.pythonPath = get_python_path(conf.root_dir)
+        end,
+      })
+      lsp_enable("texlab", {
+        settings = { texlab = { chktex = { onOpenAndSave = true } } },
+      }, function(client, bufnr)
+        ---@diagnostic disable-next-line: assign-type-mismatch
+        client.server_capabilities.completionProvider = false -- we use `vimtex` completion!
+        vim.keymap.set(
+          "n",
+          "<LocalLeader>lw",
+          "<Cmd>w<CR><Cmd>TexWordCount<CR>",
+          { noremap = true, silent = true, buffer = bufnr }
+        )
+      end)
+      lsp_enable("yamlls", {
+        settings = {
+          yaml = {
+            schemaStore = { enable = false, url = "" },
+            schemas = require("schemastore").yaml.schemas(),
           },
-        })
-      )
+        },
+      })
     end,
   },
 }
